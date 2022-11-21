43427d18369306ee034620923bb1277f08a32854
sukbonggg <kiti982611@gmail.com>
2022-11-21 21:33:44 +0900

    DB 연결

---
+++ src/main/java/com/koreaIT/java/am/ArticleListServlet.java
@@ -0,0 +1,66 @@
+package com.koreaIT.java.am;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import com.koreaIT.java.am.util.DBUtil;
+import com.koreaIT.java.am.util.SecSql;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/article/list")
+public class ArticleListServlet extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		
+		Connection conn = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패");
+		}
+
+		String url = "jdbc:mysql://127.0.0.1:3306/JSPTest?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+		try {
+			conn = DriverManager.getConnection(url, "root", "");
+			
+			DBUtil dbUtil = new DBUtil();
+			
+			SecSql sql = new SecSql();
+
+			sql.append("SELECT * FROM article");
+			
+			List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql);
+			
+			response.getWriter().append(articleRows.toString());
+
+		} catch (SQLException e) {
+			System.out.println("에러: " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+
+}
