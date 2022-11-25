d047ed0b8548325cdef2c72b8fc98bb83e6b6144
sukbonggg <kiti982611@gmail.com>
2022-11-25 20:23:46 +0900

    게시물 작성시 작성자에 관한 정보 저장

--- src/main/java/com/koreaIT/java/am/ArticleDoWriteServlet.java
+++ src/main/java/com/koreaIT/java/am/ArticleDoWriteServlet.java
@@ -15,12 +15,22 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/article/doWrite")
 public class ArticleDoWriteServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
+		
+		HttpSession session =request.getSession(); 
+		
+		
+		
+		if (session.getAttribute("loginedMemberId")==null) {
+			response.getWriter().append(String.format("<script>alert('로그인 후 이용해주세요.'); location.replace('../member/login');</script>"));
+			return;
+		}
 		
 		Connection conn = null;
 
@@ -37,8 +47,11 @@ public class ArticleDoWriteServlet extends HttpServlet {
 			String title = request.getParameter("title");
 			String body  = request.getParameter("body");
 			
+			int loginedMemberId =(int)session.getAttribute("loginedMemberId");
+			
 			SecSql sql = SecSql.from("INSERT INTO article");
 			sql.append("SET regDate=NOW()");
+			sql.append(",memberId=?",loginedMemberId);
 			sql.append(",title=?",title);
 			sql.append(",`body`=?",body);
 			
